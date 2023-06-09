f5779db8a93f4686586551a9aa8ffa5586b036e9
Jan Kohlbach <jan@dorfjungs.com>
2023-06-09 13:24:24 +0200

    ⬆️ upgrade dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: 'en'
       },
-      title: 'base',
+      title: 'nuxt3 creative base',
       link: [
         // { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
         { rel: 'icon', href: '/favicon.svg', type: 'image/svg+xml' }
@@ -15,14 +15,14 @@ export default defineNuxtConfig({
         // { rel: 'manifest', href: '/manifest.webmanifest' }
       ],
       script: [
-        { async: true, defer: true, 'data-website-id': 'dad39b61-b752-41ac-aeaf-87e504221384', src: 'https://tracking.jnkl.dev/umami.js' }
+        { async: true, defer: true, 'data-website-id': 'dad39b61-b752-41ac-aeaf-87e504221384', src: 'https://tracking.jnkl.dev/umami.js', 'data-domains': 'nuxt3-creative-base.vercel.app' }
       ],
       meta: [
         { name: 'robots', content: 'noindex,nofollow' },
         { name: 'theme-color', content: '#000000' },
-        { name: 'description', content: 'base' },
-        { name: 'og:title', content: 'base' },
-        { name: 'og:description', content: 'base' },
+        { name: 'description', content: 'minimal nuxt3 starter for creative projects, includes scss setup, gsap, lenis scroll, a native webgl setting (the cursor) and a three.js setting (the cube)' },
+        { name: 'og:title', content: 'nuxt3 creative base' },
+        { name: 'og:description', content: 'minimal nuxt3 starter for creative projects, includes scss setup, gsap, lenis scroll, a native webgl setting (the cursor) and a three.js setting (the cube)' },
         // { name: 'og:image', content: '/og-image.jpg' },
         { name: 'og:type', content: 'website' },
         { name: 'og:locale', content: 'en' }
