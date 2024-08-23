01ab97009ee6c2a43dcb837c0aa142c7ce850f0f
Jan Kohlbach <mail@jankohlbach.dev>
2024-08-23 15:32:55 +0200

    🏗️ add default layout and index page to have easier start with new projects

--- app.vue
+++ app.vue
@@ -19,24 +19,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <main>
-    <Cursor />
-    <Cube />
-    <!-- <Canvas /> -->
-    <section>
-      <h1>section 1</h1>
-    </section>
-    <section>
-      <h1>section 2</h1>
-    </section>
-    <section>
-      <h1>section 3</h1>
-    </section>
-  </main>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
-
-<style lang="scss" scoped>
-section {
-  height: 100vh;
-}
-</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <Cursor />
+    <Cube />
+    <!-- <Canvas /> -->
+    <section>
+      <h1>section 1</h1>
+    </section>
+    <section>
+      <h1>section 2</h1>
+    </section>
+    <section>
+      <h1>section 3</h1>
+    </section>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+section {
+  height: 100vh;
+}
+</style>
